2dd365fe4295c6ba786e868c54b74dd56b218290
MoisesFC <37132345+MoisesFC@users.noreply.github.com>
2023-09-07 01:53:29 -0400

    made the filter section more mobile friendly

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,22 +1,135 @@
+import React, { useState } from 'react';
 import { Form, Button, Col, Row, Container } from 'react-bootstrap';
 import Rating from './Rating';
 import { CartState } from '../context/Context';
 
 const Filters = () => {
-
     const {
-        productState: { byStock, byFastDelivery, sort, byRating, },
-        productDispatch
+        productState: { byStock, byFastDelivery, sort, byRating },
+        productDispatch,
     } = CartState();
 
+    const [showFilters, setShowFilters] = useState(false);
+    const [buttonText, setButtonText] = useState('Show Filters');
 
-    console.log(byStock, byFastDelivery, sort, byRating,);
+    const toggleFilters = () => {
+        setShowFilters(!showFilters);
+        setButtonText(showFilters ? 'Show Filters' : 'Hide Filters');
+    };
 
     return (
-        <>
-            <div >
-                <Container>
-                    <Row>
+        <Container>
+            <Row className="hide-row">
+                <Col>
+                    <span>
+                        <Form.Check
+                            inline
+                            label='Ascending'
+                            name='group1'
+                            type='radio'
+                            id={`inline-1`}
+                            onChange={() =>
+                                productDispatch({
+                                    type: 'SORT_BY_PRICE',
+                                    payload: 'lowToHigh',
+                                })
+                            }
+                            checked={sort === 'lowToHigh'}
+                        />
+                    </span>
+                </Col>
+                <Col>
+                    <span>
+                        <Form.Check
+                            inline
+                            label='Descending'
+                            name='group1'
+                            type='radio'
+                            id={`inline-2`}
+                            onChange={() =>
+                                productDispatch({
+                                    type: 'SORT_BY_PRICE',
+                                    payload: 'highToLow',
+                                })
+                            }
+                            checked={sort === 'highToLow'}
+                        />
+                    </span>
+                </Col>
+                <Col>
+                    <span>
+                        <Form.Check
+                            inline
+                            label='Include Out of Stock'
+                            name='group1'
+                            type='checkbox'
+                            id={`inline-3`}
+                            onChange={() =>
+                                productDispatch({
+                                    type: 'FILTER_BY_STOCK',
+                                })
+                            }
+                            checked={byStock}
+                        />
+                    </span>
+                </Col>
+                <Col>
+                    <span>
+                        <Form.Check
+                            inline
+                            label='Fast Delivery Only'
+                            name='group1'
+                            type='checkbox'
+                            id={`inline-4`}
+                            onChange={() =>
+                                productDispatch({
+                                    type: 'FILTER_BY_DELIVERY',
+                                })
+                            }
+                            checked={byFastDelivery}
+                        />
+                    </span>
+                </Col>
+                <Col>
+                    <span>
+                        <label style={{ paddingRight: 10 }}>Rating</label>
+                        <Rating
+                            rating={byRating}
+                            onClick={(i) =>
+                                productDispatch({
+                                    type: "FILTER_BY_RATING",
+                                    payload: i + 1,
+                                })
+                            }
+                            style={{ cursor: "pointer", color: 'white' }}
+                        />
+                    </span>
+                </Col>
+                <Col>
+                    <Button
+                        variant="light"
+                        onClick={() =>
+                            productDispatch({
+                                type: 'CLEAR_FILTERS',
+                            })
+                        }
+                    >
+                        Clear Filters
+                    </Button>
+                </Col>
+
+            </Row>
+
+            <Col xs={12} md={6}>
+                <Button
+                    variant="light"
+                    onClick={toggleFilters}
+                    className="mb-3 d-md-none"
+                >
+                    {buttonText}
+                </Button>
+                {showFilters && (
+                    <div>
                         <span>Filter Products</span>
                         <Col>
                             <span>
@@ -29,9 +142,10 @@ const Filters = () => {
                                     onChange={() =>
                                         productDispatch({
                                             type: 'SORT_BY_PRICE',
-                                            payload: 'lowToHigh'
-                                        })}
-                                    checked={sort === 'lowToHigh' ? true : false}
+                                            payload: 'lowToHigh',
+                                        })
+                                    }
+                                    checked={sort === 'lowToHigh'}
                                 />
                             </span>
                         </Col>
@@ -46,9 +160,10 @@ const Filters = () => {
                                     onChange={() =>
                                         productDispatch({
                                             type: 'SORT_BY_PRICE',
-                                            payload: 'highToLow'
-                                        })}
-                                    checked={sort === 'highToLow' ? true : false}
+                                            payload: 'highToLow',
+                                        })
+                                    }
+                                    checked={sort === 'highToLow'}
                                 />
                             </span>
                         </Col>
@@ -102,18 +217,22 @@ const Filters = () => {
                             </span>
                         </Col>
                         <Col>
-                            <Button variant="light"
+                            <Button
+                                variant="light"
                                 onClick={() =>
                                     productDispatch({
-                                        type: 'CLEAR_FILTERS'
-                                    })}
-                            >Clear Filters</Button>
+                                        type: 'CLEAR_FILTERS',
+                                    })
+                                }
+                            >
+                                Clear Filters
+                            </Button>
                         </Col>
-                    </Row>
-                </Container>
-            </div>
-        </>
-    )
-}
+                    </div>
+                )}
+            </Col>
+        </Container>
+    );
+};
 
 export default Filters;
--- src/components/SingleProducts.js
+++ src/components/SingleProducts.js
@@ -1,4 +1,4 @@
-import { Card, Button, Row, Col, Form } from 'react-bootstrap';
+import { Card, Button, Row, Col, Form, Container } from 'react-bootstrap';
 import Rating from './Rating';
 import { CartState } from '../context/Context';
 
@@ -31,80 +31,84 @@ const SingleProduct = ({ prod }) => {
 
   return (
     <div className="products">
+      <Container>
       <Card>
+        
         <Row noGutters>
-        <Col xs={12} sm={4}>
-  <Card.Img
-    variant="top"
-    src={prod.image}
-    alt={prod.name}
-    className="img-fluid"
-  />
-</Col>
-<Col xs={12} sm={4} className="d-flex flex-column align-items-center justify-content-center">
-  <Row>
-    <Card.Title>{prod.name}</Card.Title>
-  </Row>
-  <Row>
-    <Card.Text className="d-none d-md-block">{prod.description}</Card.Text>
-  </Row>
-  <Row className="justify-content-center">
-    <Rating rating={prod.rating} style={{ color: 'orange', padding: 7 }} />
-  </Row>
-</Col>
-<Col xs={12} sm={4}>
-  <Card.Body>
-    <Card.Subtitle style={{ margin: 20 }}>
-      <span id='price' className='dynamic-font'>${prod.price.toLocaleString()}</span>
-      {prod.fastDelivery ? (
-        <div>Fast Delivery</div>
-      ) : (
-        <div>4 day Delivery</div>
-      )}
-    </Card.Subtitle>
-    {isInCart ? (
-      <div>
-        <Button
-          variant="danger"
-          onClick={() =>
-            dispatch({
-              type: "REMOVE_FROM_CART",
-              payload: prod,
-            })
-          }
-        >
-          Remove from Cart
-        </Button>
-        <div className="d-flex justify-content-center" style={{ padding: '9px' }}>
-          <Form.Control
-            as="input"
-            type="number"
-            value={cart.find((p) => p.id === prod.id)?.qty || 1}
-            onChange={handleQtyChange}
-            min={1}
-            max={prod.inStock}
-            disabled={!prod.inStock}
-            style={{ width: '75px', textAlign: 'center' }}
-          />
-        </div>
-      </div>
-    ) : (
-      <Button
-        onClick={() =>
-          dispatch({
-            type: "ADD_TO_CART",
-            payload: prod,
-          })
-        }
-        disabled={!prod.inStock}
-      >
-        {!prod.inStock ? "Out of Stock" : "Add to Cart"}
-      </Button>
-    )}
-  </Card.Body>
-</Col>
+          <Col xs={12} sm={4}>
+            <Card.Img
+              variant="top"
+              src={prod.image}
+              alt={prod.name}
+              className="img-fluid"
+            />
+          </Col>
+          <Col xs={12} sm={4} className="d-flex flex-column align-items-center justify-content-center">
+            <Row>
+              <Card.Title>{prod.name}</Card.Title>
+            </Row>
+            <Row>
+              <Card.Text className="d-none d-md-block">{prod.description}</Card.Text>
+            </Row>
+            <Row className="justify-content-center">
+              <Rating rating={prod.rating} style={{ color: 'orange', padding: 7 }} />
+            </Row>
+          </Col>
+          <Col xs={12} sm={4}>
+            <Card.Body>
+              <Card.Subtitle style={{ margin: 20 }}>
+                <span id='price' className='dynamic-font'>${prod.price.toLocaleString()}</span>
+                {prod.fastDelivery ? (
+                  <div>Fast Delivery</div>
+                ) : (
+                  <div>4 day Delivery</div>
+                )}
+              </Card.Subtitle>
+              {isInCart ? (
+                <div>
+                  <Button
+                    variant="danger"
+                    onClick={() =>
+                      dispatch({
+                        type: "REMOVE_FROM_CART",
+                        payload: prod,
+                      })
+                    }
+                  >
+                    Remove from Cart
+                  </Button>
+                  <div className="d-flex justify-content-center" style={{ padding: '9px' }}>
+                    <Form.Control
+                      as="input"
+                      type="number"
+                      value={cart.find((p) => p.id === prod.id)?.qty || 1}
+                      onChange={handleQtyChange}
+                      min={1}
+                      max={prod.inStock}
+                      disabled={!prod.inStock}
+                      style={{ width: '75px', textAlign: 'center' }}
+                    />
+                  </div>
+                </div>
+              ) : (
+                <Button
+                  onClick={() =>
+                    dispatch({
+                      type: "ADD_TO_CART",
+                      payload: prod,
+                    })
+                  }
+                  disabled={!prod.inStock}
+                >
+                  {!prod.inStock ? "Out of Stock" : "Add to Cart"}
+                </Button>
+              )}
+            </Card.Body>
+          </Col>
         </Row>
+        
       </Card>
+      </Container>
 
     </div>
   );
