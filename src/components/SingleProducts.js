bef67bf3ae03f7bc2a2cc073032da0f7b0aec239
MoisesFC <37132345+MoisesFC@users.noreply.github.com>
2023-09-07 01:32:15 -0400

    made the card component more responsive to small screens

--- src/components/SingleProducts.js
+++ src/components/SingleProducts.js
@@ -33,71 +33,76 @@ const SingleProduct = ({ prod }) => {
     <div className="products">
       <Card>
         <Row noGutters>
-          <Col xs={4}>
-            <Card.Img variant="top" src={prod.image} alt={prod.name} style={{ width: '50%', height: 'auto' }} />
-          </Col>
-          <Col xs={4} className="d-flex flex-column align-items-center justify-content-center">
-            <Row>
-              <Card.Title>{prod.name}</Card.Title>
-            </Row>
-            <Row>
-              <Card.Text className="d-none d-md-block">{prod.description} </Card.Text>
-            </Row>
-            <Row className="justify-content-center">
-              <Rating rating={prod.rating} style={{ color: 'orange', padding: 7 }} />
-            </Row>
-          </Col>
-          <Col xs={4}>
-            <Card.Body >
-              <Card.Subtitle style={{ margin: 50}}>
-                <span id='price' class='dynamic-font'>${prod.price.toLocaleString()}</span>
-                {prod.fastDelivery ? (
-                  <div>Fast Delivery</div>
-                ) : (
-                  <div>4 day Delivery</div>
-                )}
-              </Card.Subtitle>
-              {isInCart ? (
-                <div>
-                  <Button
-                    variant="danger"
-                    onClick={() =>
-                      dispatch({
-                        type: "REMOVE_FROM_CART",
-                        payload: prod,
-                      })
-                    }
-                  >
-                    Remove from Cart
-                  </Button>
-                  <div className="d-flex justify-content-center" style={{ padding: '9px' }}>
-                    <Form.Control
-                      as="input"
-                      type="number"
-                      value={cart.find((p) => p.id === prod.id)?.qty || 1}
-                      onChange={handleQtyChange}
-                      min={1}
-                      max={prod.inStock}
-                      disabled={!prod.inStock}
-                      style={{ width: '75px', textAlign: 'center' }}
-                    />
-                  </div>
-                </div>
-              ) : (
-                <Button
-                  onClick={() =>
-                    dispatch({
-                      type: "ADD_TO_CART",
-                      payload: prod,
-                    })
-                  }
-                  disabled={!prod.inStock}
-                >
-                  {!prod.inStock ? "Out of Stock" : "Add to Cart"}
-                </Button>
-              )}
-            </Card.Body>
-          </Col>
+        <Col xs={12} sm={4}>
+  <Card.Img
+    variant="top"
+    src={prod.image}
+    alt={prod.name}
+    className="img-fluid"
+  />
+</Col>
+<Col xs={12} sm={4} className="d-flex flex-column align-items-center justify-content-center">
+  <Row>
+    <Card.Title>{prod.name}</Card.Title>
+  </Row>
+  <Row>
+    <Card.Text className="d-none d-md-block">{prod.description}</Card.Text>
+  </Row>
+  <Row className="justify-content-center">
+    <Rating rating={prod.rating} style={{ color: 'orange', padding: 7 }} />
+  </Row>
+</Col>
+<Col xs={12} sm={4}>
+  <Card.Body>
+    <Card.Subtitle style={{ margin: 20 }}>
+      <span id='price' className='dynamic-font'>${prod.price.toLocaleString()}</span>
+      {prod.fastDelivery ? (
+        <div>Fast Delivery</div>
+      ) : (
+        <div>4 day Delivery</div>
+      )}
+    </Card.Subtitle>
+    {isInCart ? (
+      <div>
+        <Button
+          variant="danger"
+          onClick={() =>
+            dispatch({
+              type: "REMOVE_FROM_CART",
+              payload: prod,
+            })
+          }
+        >
+          Remove from Cart
+        </Button>
+        <div className="d-flex justify-content-center" style={{ padding: '9px' }}>
+          <Form.Control
+            as="input"
+            type="number"
+            value={cart.find((p) => p.id === prod.id)?.qty || 1}
+            onChange={handleQtyChange}
+            min={1}
+            max={prod.inStock}
+            disabled={!prod.inStock}
+            style={{ width: '75px', textAlign: 'center' }}
+          />
+        </div>
+      </div>
+    ) : (
+      <Button
+        onClick={() =>
+          dispatch({
+            type: "ADD_TO_CART",
+            payload: prod,
+          })
+        }
+        disabled={!prod.inStock}
+      >
+        {!prod.inStock ? "Out of Stock" : "Add to Cart"}
+      </Button>
+    )}
+  </Card.Body>
+</Col>
         </Row>
       </Card>
 
